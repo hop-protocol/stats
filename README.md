efec2335bc7aa42172b973bbe56f759fce500b77
Miguel Mota <hello@miguelmota.com>
2021-10-03 23:00:13 -0700

    Price feed

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hop Volume
+
+> A simple page that displays Hop total volume with data from [TheGraph](https://github.com/hop-protocol/subgraph).
+
+[https://volume.hop.exchange/](https://volume.hop.exchange/)
--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
-function formatVolume (volumes) {
+function formatVolume (volumes, prices) {
   let amount = 0
   for (const volume of volumes) {
     const decimals = tokenDecimals[volume.token]
     const rawAmount = ethers.BigNumber.from(volume.amount)
-    const _amount = Number(ethers.utils.formatUnits(rawAmount, decimals))
+    const _amount = Number(ethers.utils.formatUnits(rawAmount, decimals)) * prices[volume.token]
     amount = amount + _amount
   }
   const formattedAmount = formatCurrency(amount)
@@ -74,20 +74,22 @@ async function updateVolume () {
     polygonVolume,
     optimismVolume,
     arbitrumVolume,
-    mainnetVolume
+    mainnetVolume,
+    prices
   ] = await Promise.all([
     fetchVolume('xdai'),
     fetchVolume('polygon'),
     fetchVolume('optimism'),
     fetchVolume('arbitrum'),
-    fetchVolume('mainnet')
+    fetchVolume('mainnet'),
+    getUsdPrices()
   ])
 
-  const xdai = formatVolume(xdaiVolume)
-  const polygon = formatVolume(polygonVolume)
-  const optimism = formatVolume(optimismVolume)
-  const arbitrum = formatVolume(arbitrumVolume)
-  const ethereum = formatVolume(mainnetVolume)
+  const xdai = formatVolume(xdaiVolume, prices)
+  const polygon = formatVolume(polygonVolume, prices)
+  const optimism = formatVolume(optimismVolume, prices)
+  const arbitrum = formatVolume(arbitrumVolume, prices)
+  const ethereum = formatVolume(mainnetVolume, prices)
 
   const totalAmount = xdai.amount + polygon.amount + optimism.amount + arbitrum.amount + ethereum.amount
   const total = {
@@ -107,23 +109,72 @@ async function updateVolume () {
   return volume
 }
 
-async function poll() {
+async function getUsdPrices () {
+  const baseUrl = 'https://api.coingecko.com/api/v3'
+  const tokens = {
+    USDC: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+    USDT: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+    DAI: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+    MATIC: '0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0',
+    ETH: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2' // WETH
+  }
+  const base = 'usd'
+  const addresses = Object.values(tokens)
+  const params = {
+    contract_addresses: addresses.join(','),
+    vs_currencies: base,
+    include_market_cap: false,
+    include_24hr_vol: false,
+    include_24hr_change: false,
+    include_last_updated_at: false
+  }
+
+  let qs = ''
+  for (const key in params) {
+    qs += `${key}=${params[key]}&`
+  }
+
+  const url = `${baseUrl}/simple/token_price/ethereum?${qs}`
+  const res = await fetch(url)
+  const json = await res.json()
+  const prices = {}
+
+  for (let i = 0; i < addresses.length; i++) {
+    const address = addresses[i]
+    const token = Object.keys(tokens).find(key => tokens[key] === address)
+    try {
+      const item = json[address.toLowerCase()]
+      if (!item) {
+        throw new Error('not found')
+      }
+
+      const price = Number(item[base])
+      prices[token] = price
+    } catch (err) {
+      console.error(err)
+      prices[token] = 1
+    }
+  }
+
+  return prices
+}
+
+async function poll () {
   const counter = document.getElementById('counter')
   const volume = await updateVolume()
   counter.innerText = volume.total.formattedAmount
 }
 
-async function main() {
+async function main () {
   const fetchInterval = 10 * 1000
   while (true) {
     try {
       await poll()
       await new Promise((resolve) => setTimeout(() => resolve(null), fetchInterval))
-    } catch(err){
+    } catch (err) {
       console.error(err)
     }
   }
 }
 
 main()
-
